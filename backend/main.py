2fc13a548e76a0dead1437536c28200f2202d8dd
Hawtspecta <nayaksahana0307@gmail.com>
2026-03-12 11:31:42 +0530

    deployed vercel

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", 
+                   "https://ai-resume-analyzer-dun-one.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
